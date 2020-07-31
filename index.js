3847f8e7a902a3e5bb2fdb488b61fb0e797be7c1
疯狂紫萧 <cuo9958@gmail.com>
2020-07-31 19:43:41 +0800

    增加id的索引

--- index.js
+++ index.js
@@ -32,12 +32,13 @@ function cutLog300() {
  * @param {*} start 开始索引
  * @param {*} len 字节长度
  */
-function addToIndex(title, start, len) {
+function addToIndex(title, start, len, id) {
     if (!IndexList) IndexList = []
     IndexList.unshift({
         title,
         start,
         len,
+        id,
     })
 }
 /**
@@ -52,11 +53,12 @@ function saveToCache() {
  * @param {*} str 字符串
  * @param {*} len 字节数
  */
-function addToWait(title, str, len) {
+function addToWait(title, str, len, time) {
     BackList.push({
         title,
         str,
         len,
+        time,
     })
 }
 /**
@@ -70,7 +72,7 @@ function writeWait() {
     fs.getFileInfo({
         filePath: path,
         complete: function (res) {
-            addToIndex(data.title, res.size, data.len)
+            addToIndex(data.title, res.size, data.len, data.time)
             fs.appendFileSync(path, data.str, 'utf-8')
             // console.log('加入内容长度', res.size, data.len)
             ISLOADING = false
@@ -103,16 +105,16 @@ function getByteLen(str) {
 /**
  * 测试用方法
  */
-function getFileInfo() {
-    const list = wx.getStorageSync(IndexKey)
-    console.log(path, list, list.length)
-    fs.getFileInfo({
-        filePath: path,
-        complete: function (res) {
-            console.log('获取文件信息', res.size, res.digest)
-        },
-    })
-}
+// function getFileInfo() {
+//     const list = wx.getStorageSync(IndexKey)
+//     console.log(path, list, list.length)
+//     fs.getFileInfo({
+//         filePath: path,
+//         complete: function (res) {
+//             console.log('获取文件信息', res.size, res.digest)
+//         },
+//     })
+// }
 
 /**
  * 初始化日志
@@ -151,14 +153,14 @@ export function addLog(title, data, opts, time = Date.now()) {
     const str = JSON.stringify(model)
     const len = getByteLen(str)
     if (ISLOADING) {
-        addToWait(title, str, len)
+        addToWait(title, str, len, time)
         return
     }
     ISLOADING = true
     fs.getFileInfo({
         filePath: path,
         complete: function (res) {
-            addToIndex(title, res.size, len)
+            addToIndex(title, res.size, len, time)
             fs.appendFileSync(path, str, 'utf-8')
             // console.log('加入内容长度', res.size, len)
             //超过7MB
@@ -188,14 +190,10 @@ export function clearLog() {
  * @param {*} pageIndex 开始页数,从0开始
  * @param {*} len 每页条数
  */
-export function getLogList(pageIndex, len = 20) {
-    const list = wx.getStorageSync(IndexKey)
+export function getLogList(pageIndex = 0, len = 30) {
     const start = pageIndex * len
     const end = start + len
-    const data = list.slice(start, end)
-    data.forEach((item, index) => {
-        item.id = start + index
-    })
+    const data = IndexList.slice(start, end)
     return data
 }
 /**
@@ -204,8 +202,7 @@ export function getLogList(pageIndex, len = 20) {
  */
 export function getLogInfo(id) {
     // getFileInfo()
-    const list = wx.getStorageSync(IndexKey)
-    const data = list[id]
+    const data = IndexList.find((item) => item.id + '' === id)
     if (!data) return null
     const res = fs.readFileSync(path, 'utf-8', data.start, data.len)
     try {
